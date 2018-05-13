63af9dae3704fa30ff1d3f23c2f7f761db5223ad
riccardodesanti <riccardodesantimail@gmail.com>
2018-05-13 22:51:03 +0200

    debug

--- index.js
+++ index.js
@@ -88,15 +88,16 @@ function handleMessage(sender_psid, received_message, user_first_name) {
   if (received_message.text) {
 
     let user_first_name = "asd";
+    let response = {};
     request('https://graph.facebook.com/v2.6/'+ sender_psid + '?fields=first_name,last_name&access_token=EAADErAHrZBCABAASPN5wugmSGxIGKjduZBc6DCRn5GiHLtvoKRWd2bE2QXeXBFV1MybSW1MkHaB1xNujxusWGi8au1QWiysTiR41OiwEZC4CJSbmI2IWfAxRKZBSL8BIVMCMdYFJXUF19tZBnQKZCeZC9uZC83LgvmG1t7uKsepUtgZDZD', { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       user_first_name = body.first_name;
+      // Creates the payload for a basic text messages 
+      response = {
+        "text": 'Hello '+ user_first_name +', Would you like to set an appointment?'
+      }
     });
 
-    // Creates the payload for a basic text messages
-    response = {
-      "text": 'Hello '+ user_first_name +', Would you like to set an appointment?'
-    }
   } else if (received_message.attachments) {
     // Gets the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
