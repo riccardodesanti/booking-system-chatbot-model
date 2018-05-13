148d1f5f097946b3c96a1bf0e84ac83ebc10f689
riccardodesanti <riccardodesantimail@gmail.com>
2018-05-13 17:01:14 +0200

    set dev env in Atom

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
 
+
 let body = req.body;
 
 // Checks this is an event from a page subscription
